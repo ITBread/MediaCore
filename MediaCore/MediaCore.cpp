35805d708a7e02df0d03aadb308d8d429c5075b5
ycg <yangchengguo@beidouapp.com>
2018-07-09 17:58:23 +0800

    remove cmdutils.cpp cmdutils.h and unused functions

--- MediaCore/MediaCore.cpp
+++ MediaCore/MediaCore.cpp
@@ -57,6 +57,10 @@ const int program_birth_year = 2003;
 
 static unsigned sws_flags = SWS_BICUBIC;
 /* options specified by the user */
+static AVDictionary *sws_dict;
+static AVDictionary *swr_opts;
+static AVDictionary *format_opts, *codec_opts, *resample_opts;
+
 static AVInputFormat *file_iformat;
 static const char *input_filename;
 static const char *window_title;
@@ -141,6 +145,167 @@ static const struct TextureFormatEntry {
     { AV_PIX_FMT_UYVY422,        SDL_PIXELFORMAT_UYVY },
     { AV_PIX_FMT_NONE,           SDL_PIXELFORMAT_UNKNOWN },
 };
+
+void exit_program(int ret)
+{
+
+}
+
+static int check_stream_specifier(AVFormatContext *s, AVStream *st, const char *spec)
+{
+	int ret = avformat_match_stream_specifier(s, st, spec);
+	if (ret < 0)
+		av_log(s, AV_LOG_ERROR, "Invalid stream specifier: %s.\n", spec);
+	return ret;
+}
+
+AVDictionary *filter_codec_opts(AVDictionary *opts, enum AVCodecID codec_id,
+	AVFormatContext *s, AVStream *st, AVCodec *codec)
+{
+	AVDictionary    *ret = NULL;
+	AVDictionaryEntry *t = NULL;
+	int            flags = s->oformat ? AV_OPT_FLAG_ENCODING_PARAM
+		: AV_OPT_FLAG_DECODING_PARAM;
+	char          prefix = 0;
+	const AVClass    *cc = avcodec_get_class();
+
+	if (!codec)
+		codec            = s->oformat ? avcodec_find_encoder(codec_id)
+		: avcodec_find_decoder(codec_id);
+
+	switch (st->codecpar->codec_type) {
+	case AVMEDIA_TYPE_VIDEO:
+		prefix  = 'v';
+		flags  |= AV_OPT_FLAG_VIDEO_PARAM;
+		break;
+	case AVMEDIA_TYPE_AUDIO:
+		prefix  = 'a';
+		flags  |= AV_OPT_FLAG_AUDIO_PARAM;
+		break;
+	case AVMEDIA_TYPE_SUBTITLE:
+		prefix  = 's';
+		flags  |= AV_OPT_FLAG_SUBTITLE_PARAM;
+		break;
+	}
+
+	while (t = av_dict_get(opts, "", t, AV_DICT_IGNORE_SUFFIX)) {
+		char *p = strchr(t->key, ':');
+
+		/* check stream specification in opt name */
+		if (p)
+			switch (check_stream_specifier(s, st, p + 1)) {
+			case  1: *p = 0; break;
+			case  0:         continue;
+			default:         exit_program(1);
+		}
+
+		if (av_opt_find(&cc, t->key, NULL, flags, AV_OPT_SEARCH_FAKE_OBJ) ||
+			!codec ||
+			(codec->priv_class &&
+			av_opt_find(&codec->priv_class, t->key, NULL, flags,
+			AV_OPT_SEARCH_FAKE_OBJ)))
+			av_dict_set(&ret, t->key, t->value, 0);
+		else if (t->key[0] == prefix &&
+			av_opt_find(&cc, t->key + 1, NULL, flags,
+			AV_OPT_SEARCH_FAKE_OBJ))
+			av_dict_set(&ret, t->key + 1, t->value, 0);
+
+		if (p)
+			*p = ':';
+	}
+	return ret;
+}
+
+AVDictionary **setup_find_stream_info_opts(AVFormatContext *s,
+	AVDictionary *codec_opts)
+{
+	int i;
+	AVDictionary **opts;
+
+	if (!s->nb_streams)
+		return NULL;
+	opts = ( AVDictionary **)av_mallocz_array(s->nb_streams, sizeof(*opts));
+	if (!opts) {
+		av_log(NULL, AV_LOG_ERROR,
+			"Could not alloc memory for stream options.\n");
+		return NULL;
+	}
+	for (i = 0; i < s->nb_streams; i++)
+		opts[i] = filter_codec_opts(codec_opts, s->streams[i]->codecpar->codec_id,
+		s, s->streams[i], NULL);
+	return opts;
+}
+
+void *grow_array(void *array, int elem_size, int *size, int new_size)
+{
+	if (new_size >= INT_MAX / elem_size) {
+		av_log(NULL, AV_LOG_ERROR, "Array too big.\n");
+		exit_program(1);
+	}
+	if (*size < new_size) {
+		uint8_t *tmp = (uint8_t *)av_realloc_array(array, new_size, elem_size);
+		if (!tmp) {
+			av_log(NULL, AV_LOG_ERROR, "Could not alloc buffer.\n");
+			exit_program(1);
+		}
+		memset(tmp + *size*elem_size, 0, (new_size-*size) * elem_size);
+		*size = new_size;
+		return tmp;
+	}
+	return array;
+}
+
+double get_rotation(AVStream *st)
+{
+	uint8_t* displaymatrix = av_stream_get_side_data(st,
+		AV_PKT_DATA_DISPLAYMATRIX, NULL);
+	double theta = 0;
+	if (displaymatrix)
+		theta = -av_display_rotation_get((int32_t*) displaymatrix);
+
+	theta -= 360*floor(theta/360 + 0.9/360);
+
+	if (fabs(theta - 90*round(theta/90)) > 2)
+		av_log(NULL, AV_LOG_WARNING, "Odd rotation angle.\n"
+		"If you want to help, upload a sample "
+		"of this file to ftp://upload.ffmpeg.org/incoming/ "
+		"and contact the ffmpeg-devel mailing list. (ffmpeg-devel@ffmpeg.org)");
+
+	return theta;
+}
+
+void init_opts(void)
+{
+	av_dict_set(&sws_dict, "flags", "bicubic", 0);
+}
+
+void uninit_opts(void)
+{
+	av_dict_free(&swr_opts);
+	av_dict_free(&sws_dict);
+	av_dict_free(&format_opts);
+	av_dict_free(&codec_opts);
+	av_dict_free(&resample_opts);
+}
+
+void init_dynload(void)
+{
+#ifdef _WIN32
+    /* Calling SetDllDirectory with the empty string (but not NULL) removes the
+     * current working directory from the DLL search path as a security pre-caution. */
+    SetDllDirectory(L"");
+#endif
+}
+
+static void print_error(const char *filename, int err)
+{
+	char errbuf[128];
+	const char *errbuf_ptr = errbuf;
+
+	if (av_strerror(err, errbuf, sizeof(errbuf)) < 0)
+		errbuf_ptr = strerror(AVUNERROR(err));
+	av_log(NULL, AV_LOG_ERROR, "%s: %s\n", filename, errbuf_ptr);
+}
 
 
 #if CONFIG_AVFILTER
@@ -3208,49 +3373,11 @@ static void event_loop(VideoState *cur_stream)
 }
 
 
-
-
-
 static void show_usage(void)
 {
     av_log(NULL, AV_LOG_INFO, "Simple media player\n");
     av_log(NULL, AV_LOG_INFO, "usage: %s [options] input_file\n", program_name);
     av_log(NULL, AV_LOG_INFO, "\n");
-}
-
-void show_help_default(const char *opt, const char *arg)
-{
-    av_log_set_callback(log_callback_help);
-    show_usage();
-    //show_help_options(options, "Main options:", 0, OPT_EXPERT, 0);
-    //show_help_options(options, "Advanced options:", OPT_EXPERT, 0, 0);
-    printf("\n");
-    show_help_children(avcodec_get_class(), AV_OPT_FLAG_DECODING_PARAM);
-    show_help_children(avformat_get_class(), AV_OPT_FLAG_DECODING_PARAM);
-#if !CONFIG_AVFILTER
-    show_help_children(sws_get_class(), AV_OPT_FLAG_ENCODING_PARAM);
-#else
-    show_help_children(avfilter_get_class(), AV_OPT_FLAG_FILTERING_PARAM);
-#endif
-    printf("\nWhile playing:\n"
-           "q, ESC              quit\n"
-           "f                   toggle full screen\n"
-           "p, SPC              pause\n"
-           "m                   toggle mute\n"
-           "9, 0                decrease and increase volume respectively\n"
-           "/, *                decrease and increase volume respectively\n"
-           "a                   cycle audio channel in the current program\n"
-           "v                   cycle video channel\n"
-           "t                   cycle subtitle channel in the current program\n"
-           "c                   cycle program\n"
-           "w                   cycle video filters or show modes\n"
-           "s                   activate frame-step mode\n"
-           "left/right          seek backward/forward 10 seconds\n"
-           "down/up             seek backward/forward 1 minute\n"
-           "page down/page up   seek backward/forward 10 minutes\n"
-           "right mouse click   seek to percentage in file corresponding to fraction of width\n"
-           "left double-click   toggle full screen\n"
-           );
 }
 
 static int lockmgr(void **mtx, enum AVLockOp op)
